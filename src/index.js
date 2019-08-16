b1fb6a8c89f9eff4d9c3b5b90b9ede30e787e96d
Daniil Kadzevych <daniil.kadzevych@sizmek.com>
2019-08-16 13:08:38 +0300

    Test-2

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const publicDir = path.join(__dirname, '../public');
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(publicDir));
 app.use(bodyParser.json());
 
@@ -29,6 +30,10 @@ app.use('/api/admin', adminRoutes);
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
     return res.status(500).send('Something broke!');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
 
 app.listen(port);
