e99f0aad5d163653cbfe08220525f9d10c97e929
Daniil Kadzevych <daniil.kadzevych@sizmek.com>
2019-08-18 22:41:09 +0300

    Chnged publick dir path

--- src/index.js
+++ src/index.js
@@ -13,10 +13,9 @@ const adminRoutes = require('./routes/admin/admin');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const publicDir = path.join(__dirname, '../public');
 
 app.use(express.static(path.join(__dirname, '/../client/build')));
-app.use(express.static(publicDir));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.json());
 
 app.use('/api/product', productRoutes);
